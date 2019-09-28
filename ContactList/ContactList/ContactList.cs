e3501731c8c7fd5ffa8294f39584928e48261155
Florian Hitzler <20150060@HTBLAPerg.onmicrosoft.com>
2019-09-28 12:10:21 +0200

    finished

---
+++ ContactList/ContactList/ContactList.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ContactList
+{
+    public class Contact
+    {
+        public String id { get; set; }
+        public String firstname { get; set; }
+
+        public String lastname { get; set; }
+
+        public String email { get; set; }
+    }
+}
---
+++ ContactList/ContactList/Controllers/ContactListController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ContactList;
+namespace AspNetDemoApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/contacts")]
+    public class ContactListController : ControllerBase
+    {
+        private static readonly List<Contact> contacts = new List<Contact> { };
+
+        [HttpGet]
+        public IActionResult GetAllItems() => Ok(contacts);
+        
+        [HttpGet]
+        [Route("{personId}", Name = "GetSpecificContact")]
+        public IActionResult GetItem(string personId)
+        {
+            foreach(Contact c in contacts)
+            {
+                if(personId == c.id)
+                {
+                    return Ok(c);
+                }
+            }
+            return NotFound("No Person found with that ID");
+        }
+
+        [HttpGet]
+        [Route("findByName", Name = "GetContactByName")]
+        public IActionResult GetContactByName([FromQuery] string nameFilter)
+        {
+            if (String.IsNullOrEmpty(nameFilter))
+            {
+                return BadRequest("Invalid or missing name");
+            }
+            List<Contact> filteredContacts = new List<Contact> { };
+            foreach (Contact c in contacts)
+            {
+                if(c.firstname.Contains(nameFilter) || c.lastname.Contains(nameFilter))
+                {
+                    filteredContacts.Add(c);
+                }
+            }
+            if (filteredContacts.Count > 0)
+            {
+                return Ok(filteredContacts);
+            }
+            return Ok(filteredContacts);
+            
+        }
+
+        [HttpPost]
+        public IActionResult addContact([FromBody] Contact newContact)
+        {
+
+            if(newContact.id == null || newContact.lastname == null)
+            {
+                return BadRequest("Invalid Input");
+            }
+            foreach(Contact c in contacts)
+            {
+                if(c.id == newContact.id)
+                {
+                    return BadRequest("ID already exists");
+                }
+            }
+            contacts.Add(newContact);
+            return CreatedAtRoute("GetSpecificContact", new { personId = newContact.id }, newContact);
+        }
+
+
+        [HttpDelete]
+        [Route("{personId}")]
+        public IActionResult DeleteContact(string personId)
+        {
+            if(!(int.TryParse(personId, out int n)))
+            {
+                return BadRequest("Invalid ID");
+            }
+            foreach(Contact c in contacts)
+            {
+                if(c.id == personId)
+                {
+                    contacts.Remove(c);
+                    return NoContent();
+                }
+            }
+
+            return NotFound("Person not found");
+        }
+    }
+}
